a7496fa34c1b8f52e1fd8cf236d82888a02426a1
cmacdon4 <cmacdon4@nd.edu>
2024-04-25 10:44:38 -0400

    oops forgot something

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -31,6 +31,16 @@ class _AuthenticateState extends State<Authenticate> {
     }
   }
 
+  Future<bool> sendPasswordResetEmail(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      return true; // Password reset email sent successfully
+    } catch (e) {
+      print('Error sending password reset email: $e');
+      return false; // Failed to send password reset email
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     /* Display sign in view if showSignIn is true, otherwise display register view */
